8da62e8af2dc742e63f055bcafeac850ce77d03c
Jatin <jatin.singhal@metaschool.so>
2024-11-21 08:37:14 +0530

    Add files via upload

---
+++ chromeapi/server.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "write_Password",
+  process.env.DATABASE_PASSWORD
+);
+
+
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successful!"));
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}....`);
+});
